c3d511d59905cf00430cbbb07607ab6a40f5ebbf
Iviwe <Fistozvoti@gmail.com>
2019-09-09 16:00:49 +0200

    initial commit

--- greet_filton.js
+++ greet_filton.js
@@ -9,3 +9,15 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgBlue.black(greet('Filton'));
 console.log(styledMessage)
+
+// using figlet
+const figlet = require('figlet');
+
+figlet('Filton!', function(errorMsg, data) {
+    if (errorMsg) {
+        console.log('Something went wrong...');
+        console.dir(errorMsg);
+        return;
+    }
+    console.log(data)
+});
